f83f5bd88886418ec2369a3696749724dd11dc2b
Saif Allah DAKHLAOUI <saifallah-dakhlaoui>
2024-12-18 20:39:01 +0100

    fix find function

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ import userRouter from './src/modules/users/user.routes.js'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+//Middleware
+app.use(express.json())
 app.use(userRouter)
+
+
+
 dbConnection()
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -4,9 +4,26 @@ import { userModel } from "../../../models/user.model.js"
 
 
 const signUp = async(req,res) => {
-    const {name,email,password} = req.body
-  await userModel.insertMany({name,email,password})
-  res.json({message : "success"})
+  //Send data
+  const {name,email,password} = req.body
+  //await userModel.insertMany({name,email,password})
+  //^Check if user exist or not :
+  const user =await userModel.findOne({email})
+  if(user){
+    return res.json({message : "user already exist"})
+  }else{
+    await userModel.insertMany({name,email,password})
+    res.json({message : "success",})
+  }
+
+
+  //^explication :
+  //Retrieve data by find ===>  Array
+  // let users = await userModel.find({name :"saif"},{password :0})
+   //Retrieve data by findOne ==> object or null
+  //  let users = await userModel.findOne({name :"saif11"})
+  //find({email :"6762dc95b754ae7875b20910"}, {password :0})==> exclude password
+  //res.json({message : "success",})
 }
 
 export{
